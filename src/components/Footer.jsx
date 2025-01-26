dafdbabff9de4c83d1bfc7f116f7e489f67a1b4f
Wilman Conde <willconde2@hotmail.com>
2025-01-26 18:20:55 -0500

    --amend

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,41 @@
-const dateTime = () => {
-  const now = new Date();
+// src/components/Footer.jsx
 
-  const day = String(now.getDate()).padStart(2, '0');
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const year = String(now.getFullYear()).slice(-2);
-
-  let hours = now.getHours();
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12 || 12;
-
-  return `${day}/${month}/${year} ${hours}:${minutes} ${ampm}`;
-};
+const year = new Date().getFullYear();
 
 const Footer = () => {
   return (
     <>
       <div className='footer-fixed'>
-        <span>{dateTime()} | WC2 | willconde2@hotmail.com</span>
+        © {year} &middot; by{' '}
+        <a
+          className='kraken'
+          href='https://krakendigitalsd.netlify.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          KrakenDigitalSD
+        </a>{' '}
+        &middot;
+        <div id='redesSociales'>
+          <a href={'https://github.com/wilmanconde2'} target='_blank' rel='noopener noreferrer'>
+            <img
+              className='redesSociales'
+              src='/githubWhite.png'
+              alt='Github icons created by -Artist - Flaticon'
+            />
+          </a>
+          <a
+            href={'https://www.linkedin.com/in/wilman-conde/'}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='redesSociales'
+              src='/linkedinWhite.png'
+              alt='Linkedin icons created by Smashicons - Flaticon'
+            />
+          </a>
+        </div>
       </div>
     </>
   );
